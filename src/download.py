b9e389d8b42e0d146535f0cac3d62fef8676e8fd
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-09-11 11:53:04 +0100

    cleanup debug

--- src/download.py
+++ src/download.py
@@ -39,12 +39,7 @@ def fetch_download_links(enterprise_slug, day, token, org=""):
     response = requests.get(url, headers=headers, params={"day": day}, timeout=30)
     response.raise_for_status()
 
-    print(f"Response from ${url}: ${response}")
-
-    try:
-        download_links = response.json().get("download_links", [])
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    print(f"Response from {url}: {response}")
 
     return response.json().get("download_links", [])
 
